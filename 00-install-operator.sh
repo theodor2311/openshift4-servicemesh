f170bfc6780f12b5e38e1d551a1f3933a90ad762
theodor2311 <theodor2311@gmail.com>
2020-11-27 23:48:00 +0800

    Update install-operator.sh

--- 00-install-operator.sh
+++ 00-install-operator.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Creating subscriptions..."
+echo "Installing Operators..."
 
+if ! oc get sub elasticsearch-operator -n openshift-operators-redhat >/dev/null 2>&1
+then
 oc create -f - << EOF
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
   name: elasticsearch-operator
-  namespace: openshift-operators
+  namespace: openshift-operators-redhat
 spec:
   channel: "$(oc get packagemanifest elasticsearch-operator -n openshift-marketplace -o jsonpath='{.status.defaultChannel}')"
   installPlanApproval: Automatic
@@ -16,45 +18,31 @@ spec:
   source: "$(oc get packagemanifest elasticsearch-operator -n openshift-marketplace -o jsonpath='{.status.catalogSource}')"
   sourceNamespace: "$(oc get packagemanifest elasticsearch-operator -n openshift-marketplace -o jsonpath='{.status.catalogSourceNamespace}')"
 EOF
+else
+  echo "elasticsearch-operator installed already."
+fi
 
+
+for OPERATOR in "jaeger-product" "kiali-ossm" "servicemeshoperator"
+do
+if ! oc get sub ${OPERATOR} -n openshift-operators >/dev/null 2>&1
+then
 oc create -f - << EOF
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
-  name: jaeger-product
+  name: ${OPERATOR}
   namespace: openshift-operators
 spec:
-  channel: "$(oc get packagemanifest jaeger-product -n openshift-marketplace -o jsonpath='{.status.defaultChannel}')"
+  channel: "$(oc get packagemanifest ${OPERATOR} -n openshift-marketplace -o jsonpath='{.status.defaultChannel}')"
   installPlanApproval: Automatic
-  name: jaeger-product
-  source: "$(oc get packagemanifest jaeger-product -n openshift-marketplace -o jsonpath='{.status.catalogSource}')"
-  sourceNamespace: "$(oc get packagemanifest jaeger-product -n openshift-marketplace -o jsonpath='{.status.catalogSourceNamespace}')"
+  name: ${OPERATOR}
+  source: "$(oc get packagemanifest ${OPERATOR} -n openshift-marketplace -o jsonpath='{.status.catalogSource}')"
+  sourceNamespace: "$(oc get packagemanifest ${OPERATOR} -n openshift-marketplace -o jsonpath='{.status.catalogSourceNamespace}')"
 EOF
+else
+  echo "${OPERATOR} installed already."
+fi
+done
 
-oc create -f - << EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: kiali-ossm
-  namespace: openshift-operators
-spec:
-  channel: "$(oc get packagemanifest kiali-ossm -n openshift-marketplace -o jsonpath='{.status.defaultChannel}')"
-  installPlanApproval: Automatic
-  name: kiali-ossm
-  source: "$(oc get packagemanifest kiali-ossm -n openshift-marketplace -o jsonpath='{.status.catalogSource}')"
-  sourceNamespace: "$(oc get packagemanifest kiali-ossm -n openshift-marketplace -o jsonpath='{.status.catalogSourceNamespace}')"
-EOF
-
-oc create -f - << EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: servicemeshoperator
-  namespace: openshift-operators
-spec:
-  channel: "$(oc get packagemanifest servicemeshoperator -n openshift-marketplace -o jsonpath='{.status.defaultChannel}')"
-  installPlanApproval: Automatic
-  name: servicemeshoperator
-  source: "$(oc get packagemanifest servicemeshoperator -n openshift-marketplace -o jsonpath='{.status.catalogSource}')"
-  sourceNamespace: "$(oc get packagemanifest servicemeshoperator -n openshift-marketplace -o jsonpath='{.status.catalogSourceNamespace}')"
-EOF
+echo "Operators installation finished."
