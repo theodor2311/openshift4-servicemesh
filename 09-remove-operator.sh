2f402580b442f8ad36a2025970e1fb2ad6366b37
Theodor Li <50130439+theodor2311@users.noreply.github.com>
2020-11-27 23:54:11 +0800

    Update 09-remove-operator.sh
    Will not remove Elasticsearch operator for safety.

--- 09-remove-operator.sh
+++ 09-remove-operator.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+
 set -e
 
-oc get sub elasticsearch-operator -n openshift-operators >/dev/null
 oc get sub jaeger-product -n openshift-operators >/dev/null
 oc get sub kiali-ossm -n openshift-operators >/dev/null
 oc get sub servicemeshoperator -n openshift-operators >/dev/null
 
-oc delete sub elasticsearch-operator -n openshift-operators
 oc delete sub jaeger-product -n openshift-operators
 oc delete sub kiali-ossm -n openshift-operators
 oc delete sub servicemeshoperator -n openshift-operators
