5774166be063eb3e8ca81ab316cf33b906ff585e
theodor2311 <theodor2311@gmail.com>
2020-12-02 21:52:09 +0800

    Clean up unused scrtips

--- 15-bookinfo-remove-policy-mtls.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [[ -z ${BOOKINFO_PROJECT} ]]; then
-  BOOKINFO_PROJECT='bookinfo'
-fi
-
-oc delete policy default -n $BOOKINFO_PROJECT 
--- 18-bookinfo-remove-vs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [[ -z ${BOOKINFO_PROJECT} ]]; then
-  BOOKINFO_PROJECT='bookinfo'
-fi
-
-oc delete vs details productpage ratings reviews -n $BOOKINFO_PROJECT 
