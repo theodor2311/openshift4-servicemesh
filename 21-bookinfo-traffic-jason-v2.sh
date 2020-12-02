5d0d0bb3220f073f63302be601a8bad661a19f74
theodor2311 <theodor2311@gmail.com>
2020-12-03 00:56:32 +0800

    Update 21-bookinfo-traffic-jason-v2.sh

--- 21-bookinfo-traffic-jason-v2.sh
+++ 21-bookinfo-traffic-jason-v2.sh
@@ -5,7 +5,17 @@ if [[ -z ${BOOKINFO_PROJECT} ]]; then
   BOOKINFO_PROJECT='bookinfo'
 fi
 
-ISTIO_RELEASE=$(curl --silent https://api.github.com/repos/istio/istio/releases/latest |grep -Po '"tag_name": "\K.*?(?=")')
+if [[ -z ${ISTIO_RELEASE} ]]; then
+  ISTIO_RELEASE=$(curl --silent https://api.github.com/repos/istio/istio/releases/latest |grep -Po '"tag_name": "\K.*?(?=")')
+fi
+
+# Clean up dr, gw and vs
+oc delete dr,gw,vs --all -n $BOOKINFO_PROJECT >/dev/null 2>&1
+
+# Apply Default Gateway
+oc apply -n $BOOKINFO_PROJECT -f https://raw.githubusercontent.com/istio/istio/${ISTIO_RELEASE}/samples/bookinfo/networking/bookinfo-gateway.yaml
+# Apply Default DR
+oc apply -n $BOOKINFO_PROJECT -f https://raw.githubusercontent.com/istio/istio/${ISTIO_RELEASE}/samples/bookinfo/networking/destination-rule-all.yaml
 
 oc apply -n $BOOKINFO_PROJECT -f https://raw.githubusercontent.com/istio/istio/${ISTIO_RELEASE}/samples/bookinfo/networking/virtual-service-all-v1.yaml
 oc apply -n $BOOKINFO_PROJECT -f https://raw.githubusercontent.com/istio/istio/${ISTIO_RELEASE}/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
@@ -13,7 +23,8 @@ oc apply -n $BOOKINFO_PROJECT -f https://raw.githubusercontent.com/istio/istio/$
 if [[ -z ${GATEWAY_URL} ]]; then
   GATEWAY_URL="http://$(oc -n istio-system get route istio-ingressgateway -o jsonpath='{.spec.host}')"
 fi
-printf """On the /productpage of the Bookinfo app, log in as user jason.\n
-URL: $GATEWAY_URL/productpage.\n
+printf """Instructions:
+URL: $GATEWAY_URL/productpage.
+On the /productpage of the Bookinfo app, log in as user jason without the password.
 The star ratings appear next to each review.
 """
