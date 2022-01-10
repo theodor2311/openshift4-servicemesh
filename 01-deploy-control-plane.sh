b917a61d8ad0de941214b9b161da286e1fe35e2f
Theodor Li <theodor2311@gmail.com>
2022-01-10 23:12:19 +0800

    Update 01-deploy-control-plane.sh

--- 01-deploy-control-plane.sh
+++ 01-deploy-control-plane.sh
@@ -18,7 +18,7 @@ oc get sub servicemeshoperator -n openshift-operators >/dev/null
 echo 'Waiting for opertaors...'
 
 for operator in Elasticsearch Jaeger Kiali servicemesh;do
-while [[ $(oc get csv -n ${ISTIO_PROJECT} $(oc get csv -n ${ISTIO_PROJECT} |grep $operator | awk '{print $1}') -o jsonpath='{.status.phase}') != 'Succeeded' ]];do
+while [[ $(oc get csv -n ${ISTIO_PROJECT} $(oc get csv -n ${ISTIO_PROJECT} |grep -i $operator | awk '{print $1}') -o jsonpath='{.status.phase}') != 'Succeeded' ]];do
   sleep 1
 done
 echo $operator installed
